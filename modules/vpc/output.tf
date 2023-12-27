b2a12156f2bdd75ae7303ff462e9dd4c8dd9dafc
Pincle Soto <lpinclesoto@deloitte.com>
2023-12-27 14:27:39 -0600

    vpc name

--- modules/vpc/output.tf
+++ modules/vpc/output.tf
@@ -1,7 +1,7 @@
 output "vpc_id" {
-  value = module.myapp-vpc.vpc_id
+  value = module.k8s-vpc.vpc_id
 }
 
 output "private_subnets" {
-  value = module.myapp-vpc.private_subnets 
+  value = module.k8s-vpc.private_subnets 
 }
