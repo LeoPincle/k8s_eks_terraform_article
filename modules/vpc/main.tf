8ea172d29e50bd2ed203e0904c65a43e289f6460
LeoPincle <111757137+LeoPincle@users.noreply.github.com>
2023-12-27 14:08:37 -0600

    Add files via upload

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,7 +1,7 @@
 data "aws_availability_zones" "azs" {}
-module "myapp-vpc" {
+module "k8s-vpc" {
   source          = "terraform-aws-modules/vpc/aws"
-  version         = "3.19.0"
+  version = "5.4.0"
   name            = "kubernetes-vpc"
   cidr            = var.vpc_cidr
   private_subnets = var.private_subnet_cidr_blocks
